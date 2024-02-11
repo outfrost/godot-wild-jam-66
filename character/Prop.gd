67ebf78dc46def1e249c98274683c43b8ce575fa
outfrost <kotlet.bahn@gmail.com>
2024-02-11 05:16:28 +0100

    Implement visual movement interpolation for Prop

--- character/Prop.gd
+++ character/Prop.gd
@@ -1,5 +1,7 @@
 extends CharacterBody3D
 
+@export var visual: Node3D
+
 @export var speed = 1.0
 @export var acceleration = 4.0
 @export var deceleration = 10.0
@@ -10,8 +12,15 @@ extends CharacterBody3D
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
+var movement_delta: = Vector3.ZERO
+
+func _process(delta: float) -> void:
+	var displacement = transform.basis.inverse() * movement_delta
+	visual.position = displacement * (-1.0 + Engine.get_physics_interpolation_fraction())
 
 func _physics_process(delta: float) -> void:
+	var last_pos: = transform.origin
+
 	if !is_on_floor():
 		velocity.y -= gravity * delta
 
@@ -33,6 +42,8 @@ func _physics_process(delta: float) -> void:
 	velocity.z = velocity_xz.y
 
 	move_and_slide()
+
+	movement_delta = transform.origin - last_pos
 
 #func _unhandled_input(event):
 	#if event is InputEventMouseMotion:
