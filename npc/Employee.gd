dcba94ebc0afc6b9ab3125aac5f5ff47ce6da689
outfrost <kotlet.bahn@gmail.com>
2024-02-12 02:37:49 +0100

    Create basic employee

---
+++ npc/Employee.gd
@@ -0,0 +1,11 @@
+extends Node3D
+
+@export_group("Normal", "normal_")
+@export_subgroup("Detection Range", "normal_detection_range_")
+@export var normal_detection_range_movement: = 10.0
+@export var normal_detection_range_environment: = 6.0
+
+@export_group("Chase", "chase_")
+@export_subgroup("Detection Range", "chase_detection_range_")
+@export var chase_detection_range_movement: = 20.0
+@export var chase_detection_range_environment: = 10.0
