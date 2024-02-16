ba134ffa18403b47477c375521aeaba082c77666
marekdomagala <45537789+marekdomagala@users.noreply.github.com>
2024-02-16 17:34:47 +0100

    Add Diegetic SFX and Voiceover

--- game/menu/sfx_ui-click.gd
+++ game/menu/sfx_ui-click.gd
@@ -3,23 +3,23 @@ extends FmodEventEmitter2D
 
 
 func _on_play_button_pressed():
-	%sfx_uiclick.play()
+	%UI_Click.play()
 
 func _on_credits_button_pressed():
-	%sfx_uiclick.play()
+	%UI_Click.play()
 	
 func _on_close_button_pressed():
-	%sfx_uiclick.play()
+	%UI_Click.play()
 
 func _on_choose_level_button_pressed():
-	%sfx_uiclick.play()
+	%UI_Click.play()
 
 func _on_options_button_pressed():
-	%sfx_uiclick.play()
+	%UI_Click.play()
 
 func _on_quit_button_pressed():
-	%sfx_uiclick.play()
+	%UI_Click.play()
 
 func _physics_process(_delta: float) -> void:
 	if Input.is_action_just_pressed("menu"):
-		%sfx_uiclick.play()
+		%UI_Click.play()
