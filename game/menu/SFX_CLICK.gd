b35ba7df605249b4e70f704d2e9cc88a7ecd0a14
marekdomagala <45537789+marekdomagala@users.noreply.github.com>
2024-02-26 17:36:32 +0100

    Update and connect Click SFX to all buttons, add temporary new jump SFX

--- game/menu/SFX_CLICK.gd
+++ game/menu/SFX_CLICK.gd
@@ -3,23 +3,30 @@ extends FmodEventEmitter2D
 
 
 func _on_play_button_pressed():
-	%UI_Click.play()
+	%SFX_Click.play()
 
 func _on_credits_button_pressed():
-	%UI_Click.play()
+	%SFX_Click.play()
 	
 func _on_close_button_pressed():
-	%UI_Click.play()
+	%SFX_Click.play()
 
 func _on_choose_level_button_pressed():
-	%UI_Click.play()
+	%SFX_Click.play()
 
 func _on_options_button_pressed():
-	%UI_Click.play()
+	%SFX_Click.play()
 
 func _on_quit_button_pressed():
-	%UI_Click.play()
+	%SFX_Click.play()
 
 func _physics_process(_delta: float) -> void:
 	if Input.is_action_just_pressed("menu"):
-		%UI_Click.play()
+		%SFX_Click.play()
+
+func _on_rich_text_label_meta_clicked(meta):
+	%SFX_Click.play()
+
+
+func _on_rich_text_label_2_meta_clicked(meta):
+	%SFX_Click.play()
